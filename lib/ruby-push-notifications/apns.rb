90ed8139dee3a340fba0562bb883d31f1fe665c6
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2015-02-20 20:20:14 +0000

    commented requires

--- lib/ruby-push-notifications/apns.rb
+++ lib/ruby-push-notifications/apns.rb
@@ -4,6 +4,6 @@ module RubyPushNotifications::APNS
 end
 
 require 'ruby-push-notifications/apns/apns_connection'
-require 'ruby-push-notifications/apns/apns_pusher'
-require 'ruby-push-notifications/apns/apns_notification'
+#require 'ruby-push-notifications/apns/apns_pusher'
+#require 'ruby-push-notifications/apns/apns_notification'
 
