269cc51e4aef52d0eb736ae1c8e7b5a07cbf7456
Carlos Alonso <carlos.alonso@mydrivesolutions.com>
2015-03-17 22:05:10 +0000

    managing same message for several tokens

--- lib/ruby-push-notifications/apns/apns_notification.rb
+++ lib/ruby-push-notifications/apns/apns_notification.rb
@@ -7,33 +7,37 @@ module RubyPushNotifications
 
       WEEKS_4 = 2419200 # 4 weeks
 
-      def initialize(token, data)
-        @token = token
+      def initialize(tokens, data)
+        @tokens = tokens
         @data = data
       end
 
-      def to_apns_binary(id)
-        # Notification = 2(1), FrameLength(4), items(FrameLength)
-        # Item = ItemID(1), ItemLength(2), data(ItemLength)
-        # Items:
-        # Device Token => Id: 1, length: 32, data: binary device token
-        # Payload => Id: 2, length: ??, data: json formatted payload
-        # Notification ID => Id: 3, length: 4, data: notif id as int
-        # Expiration Date => Id: 4, length: 4, data: Unix timestamp as int
-        # Priority => Id: 5, length: 1, data: 10 as 1 byte int
-        bytes = device_token + payload + notification_id(id) + expiration_date + priority
-        [2, bytes.bytesize, bytes].pack 'cNa*'
+      def each_message(starting_id)
+        @tokens.each_with_index do |token, i|
+          # Notification = 2(1), FrameLength(4), items(FrameLength)
+          # Item = ItemID(1), ItemLength(2), data(ItemLength)
+          # Items:
+          # Device Token => Id: 1, length: 32, data: binary device token
+          # Payload => Id: 2, length: ??, data: json formatted payload
+          # Notification ID => Id: 3, length: 4, data: notif id as int
+          # Expiration Date => Id: 4, length: 4, data: Unix timestamp as int
+          # Priority => Id: 5, length: 1, data: 10 as 1 byte int
+          bytes = device_token(token) + payload + notification_id(starting_id + i) + expiration_date + priority
+          yield [2, bytes.bytesize, bytes].pack 'cNa*'
+        end
       end
 
       private
 
-      def device_token
-        [1, 32, @token].pack 'cnH64'
+      def device_token(token)
+        [1, 32, token].pack 'cnH64'
       end
 
       def payload
-        json = JSON.dump(@data).force_encoding 'ascii-8bit'
-        [2, json.bytesize, json].pack 'cna*'
+        @encoded_payload ||= -> {
+            json = JSON.dump(@data).force_encoding 'ascii-8bit'
+            [2, json.bytesize, json].pack 'cna*'
+          }.call
       end
 
       def notification_id(id)
--- spec/ruby-push-notifications/apns/apns_notification_spec.rb
+++ spec/ruby-push-notifications/apns/apns_notification_spec.rb
@@ -2,23 +2,30 @@
 module RubyPushNotifications
   module APNS
     describe APNSNotification do
-      describe '#to_apns_binary' do
-        let(:device_token) { '12' }
+      describe 'building messages for each token' do
+
+        def apns_encode(json, token, id)
+          [
+            2, 56+json.bytesize, 1, 32, token, 2, json.bytesize, json,
+            3, 4, id, 4, 4, (Time.now + APNSNotification::WEEKS_4).to_i, 5, 1, 10
+          ].pack 'cNcnH64cna*cnNcnNcnc'
+        end
+
+        let(:device_tokens) { ['12', '34'] }
         let(:data) { { a: 1 } }
-        let(:notification) { APNSNotification.new device_token, data }
+        let(:notification) { APNSNotification.new device_tokens, data }
         let(:notif_id) { 1 }
 
         it 'successfully creates the apns binary' do
           json = JSON.dump data
-          expect(notification.to_apns_binary notif_id).to eq [
-            2, 56+json.bytesize, 1, 32, device_token, 2, json.bytesize, json,
-            3, 4, notif_id, 4, 4, (Time.now + APNSNotification::WEEKS_4).to_i, 5, 1, 10
-          ].pack 'cNcnH64cna*cnNcnNcnc'
+          expect do |b|
+            notification.each_message notif_id, &b
+          end.to yield_successive_args apns_encode(json, device_tokens[0], notif_id), apns_encode(json, device_tokens[1], notif_id+1)
         end
 
         it 'validates the data'
 
-        it 'validates the token'
+        it 'validates the tokens'
       end
     end
   end
