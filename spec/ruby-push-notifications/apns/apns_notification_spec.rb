71fa18abe3f638a905e07b66568e74b88f3e3ddf
waqar-baig <waqar.baig@ymail.com>
2016-02-21 21:38:23 +0530

    stubed hash object to receive #responds_to? with :to_json

--- spec/ruby-push-notifications/apns/apns_notification_spec.rb
+++ spec/ruby-push-notifications/apns/apns_notification_spec.rb
@@ -16,8 +16,8 @@ module RubyPushNotifications
       end
 
       it 'caches the payload' do
-        allow(data).to receive(:respond_to?).and_return(false)
         expect(JSON).to receive(:dump).with(data).once.and_return 'dummy string'
+        allow(data).to receive(:respond_to?).with(:to_json).and_return(false)
         notification.each_message(notif_id) {}
       end
 
