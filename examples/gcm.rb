f7317dc17bd93924656d225ae5d037f0940382db
Kjel Delaey <kjel_delaey@hotmail.com>
2017-03-13 20:51:19 +0100

    Update the GCM example file

--- examples/gcm.rb
+++ examples/gcm.rb
@@ -15,6 +15,9 @@ pusher = RubyPushNotifications::GCM::GCMPusher.new "Your app's GCM key"
 # Open and read timeouts default to 30s
 # pusher = RubyPushNotifications::GCM::GCMPusher.new "Your app's GCM key", { open_timeout: 10, read_timeout: 10 }
 
+# Use a custom GCM endpoint (e.g. useful for load testing)
+# pusher = RubyPushNotifications::GCM::GCMPusher.new "Your app's GCM key", { gcm_url: "https://example.com/gcm/send" }
+
 pusher.push [notification]
 p 'Notification sending results:'
 p "Success: #{notification.success}, Failed: #{notification.failed}"
