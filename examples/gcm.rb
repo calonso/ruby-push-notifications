88edbf752e2b15e5d45f1cdc2a8edff438d50519
Kjel Delaey <kjel_delaey@hotmail.com>
2017-03-13 21:50:11 +0100

    Minor improvement to the :gcm_url documentation

--- examples/gcm.rb
+++ examples/gcm.rb
@@ -15,7 +15,7 @@ pusher = RubyPushNotifications::GCM::GCMPusher.new "Your app's GCM key"
 # Open and read timeouts default to 30s
 # pusher = RubyPushNotifications::GCM::GCMPusher.new "Your app's GCM key", { open_timeout: 10, read_timeout: 10 }
 
-# Use a custom GCM endpoint (e.g. useful for load testing)
+# Manually set GCM URL (e.g. useful for load testing)
 # pusher = RubyPushNotifications::GCM::GCMPusher.new "Your app's GCM key", { gcm_url: "https://example.com/gcm/send" }
 
 pusher.push [notification]
